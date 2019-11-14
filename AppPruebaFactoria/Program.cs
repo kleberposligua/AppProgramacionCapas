1e9ed2666750bdc000e96243d8e490b1422df04a
kleberposligua <kleberposligua@hotmail.com>
2019-11-14 10:28:52 -0500

    Prueba función fact en app de consola

---
+++ AppPruebaFactoria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPruebaFactoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            double res = AppProgramacionCapas.Factorial.fact(num);
+            Console.WriteLine("El factorial de {0} es: {1} ",num, res);
+            Console.ReadKey();//pausa
+        }
+    }
+}
